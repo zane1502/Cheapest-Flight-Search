c6a9865663d7c7c0bc4146228641013c000ed20f
SamuelAchilike <samidope15@gmail.com>
2025-10-15 13:31:39 +0100

    Tested to see whether I can make post requests and add new people to the users sheet

--- test_sheetsDB.py
+++ test_sheetsDB.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 # ------- SheetDB -------
 APP_ID = "ishy23zt006uy"
@@ -6,53 +6,59 @@ SHEET_DB_ENDPOINT = "https://sheetdb.io/api/v1/ishy23zt006uy"
 AUTH_TOKEN = "py8y7l9tpwxnaizap733qbha7v7efozd0ypjyvkk"
 # -----------------------
 
-
-# ........... AMADEUS FLIGHT SEARCH ...........
-AMADEUS_CITIES_URL = 'https://test.api.amadeus.com/v1/reference-data/locations/cities'
-API_KEY = 'kIOt0ww30zdmV4Xk6hkQ2hHVaqzkzbJS'
-API_SECRET = 'OSUVGAihIZFqucb8'
-OAUTH_URL = 'https://test.api.amadeus.com/v1/security/oauth2/token'
-
-grant_type = 'client_credentials'
-client_data = {
-        'client_id' : API_KEY,
-        'client_secret': API_SECRET,
-        'grant_type': grant_type
-    }
-auth_response = requests.post(url=OAUTH_URL, data= client_data)
-auth_token = auth_response.json()['access_token']
-
-header = {'Authorization': f'Bearer {auth_token}'}
-
-#..............................................
-response = requests.get(url= SHEET_DB_ENDPOINT)
-
 sheet_db_header = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
     'Authorization': f'Bearer {AUTH_TOKEN}'
           }
 
-get_data = requests.get(url= SHEET_DB_ENDPOINT, headers=sheet_db_header)
-json_data = get_data.json()
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+email = input('What is your email address? ')
+email_confirm = input('Enter Your email again please: ')
 
-for i in range(len(json_data)):
-    city_name = json_data[i]['City']
-    lowest_price = json_data[i]['Lowest Price']
 
-    param = {'keyword':city_name,
-             'max': 1}
+if email == email_confirm:
+    payload = {
+        'data' :
+                    {'id': 'INCREMENT',
+                     'First Name': first_name,
+                      'Last Name': last_name,
+                      'Email': email
+                      }
 
-    city_data = requests.get(url=f'{AMADEUS_CITIES_URL}', params= param, headers=header)
-    iata_code = city_data.json()['data'][0]['iataCode']
-    data = [{
-        'City': city_name,
-        'IATA Code': iata_code,
-        'Lowest Price': lowest_price
-    }]
+    }
+    params = {'sheet': 'users'}
 
-    put_IATA_CODE = requests.put(url=f'{SHEET_DB_ENDPOINT}/City/{city_name}', headers= sheet_db_header, json= data)
-    print(put_IATA_CODE.status_code)
+    enter_details = requests.post(url=SHEET_DB_ENDPOINT, params= params, headers= sheet_db_header, data= json.dumps(payload))
 
-# new_entry = requests.put(url= f"{SHEET_DB_ENDPOINT}/id/5", headers=header, json=data)
-# print(new_entry.status_code)
+    if enter_details.status_code == 201:
+        print("You're in the club!")
+    else:
+        print(f"Error: {enter_details.status_code} -> {enter_details.text}")
+
+    get_data = requests.get(url= SHEET_DB_ENDPOINT, headers=sheet_db_header, params= params)
+    json_data = get_data.json()
+
+
+
+# for i in range(len(json_data)):
+#     city_name = json_data[i]['City']
+#     lowest_price = json_data[i]['Lowest Price']
+#
+#     param = {'keyword':city_name,
+#              'max': 1}
+#
+#     city_data = requests.get(url=f'{AMADEUS_CITIES_URL}', params= param, headers=header)
+#     iata_code = city_data.json()['data'][0]['iataCode']
+#     data = [{
+#         'City': city_name,
+#         'IATA Code': iata_code,
+#         'Lowest Price': lowest_price
+#     }]
+#
+#     put_IATA_CODE = requests.put(url=f'{SHEET_DB_ENDPOINT}/City/{city_name}', headers= sheet_db_header, json= data)
+#     print(put_IATA_CODE.status_code)
+#
+# # new_entry = requests.put(url= f"{SHEET_DB_ENDPOINT}/id/5", headers=header, json=data)
+# # print(new_entry.status_code)
